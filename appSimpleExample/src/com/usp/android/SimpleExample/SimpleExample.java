938020bacd49886d7389b14475333ca6174b56a4
Siqueira <rodrigosiqueiramelo@gmail.com>
2017-04-04 09:59:57 -0300

    Added basic features

--- appSimpleExample/src/com/usp/android/SimpleExample/SimpleExample.java
+++ appSimpleExample/src/com/usp/android/SimpleExample/SimpleExample.java
@@ -4,12 +4,20 @@ import android.view.View;
 
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class SimpleExample extends Activity
 {
+    ArrayList<String> names = new ArrayList<String>();
+    ListView listName;
+    ArrayAdapter adapter;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -20,6 +28,25 @@ public class SimpleExample extends Activity
 
     public void storeDataAndUpdate(View pView)
     {
+      EditText enteredName = (EditText) findViewById(R.id.name);
+      String newName = enteredName.getText().toString();
+
       setContentView(R.layout.list_name);
+      this.listName = (ListView) findViewById(R.id.name_list);
+
+      names.add(newName);
+      this.setAdapterToList();
+    }
+
+    public void back(View pView)
+    {
+      setContentView(R.layout.main);
+    }
+
+    private void setAdapterToList()
+    {
+      this.adapter = new ArrayAdapter<String>(this, R.layout.row,
+                                              R.id.nameText, this.names);
+      this.listName.setAdapter(this.adapter);
     }
 }
